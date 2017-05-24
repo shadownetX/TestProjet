4e1fe6404891dc96fac2da9ad71106007cd177d3
vonP <thomas@thiebaudet.fr>
2017-05-24 16:10:04 +0200

    TT: check randomWare / check calculator

--- tests/welcome.php
+++ tests/welcome.php
@@ -5,7 +5,8 @@ use Tools\Generator\TokenGenerator as Generator;
 
 require_once "../vendor/autoload.php";
 
-$ajouter = Calculette::ajouter(3,2);
+
+$ajouter = Calculette::ajouter(-3,-2);
 $soustraire = Calculette::soustraire(5,2);
 $multiplier = Calculette::multiplier(5,2);
 $diviser = Calculette::diviser(10,2);
@@ -14,4 +15,3 @@ $diviserError = Calculette::diviser(1,0);
 $generator = new Generator();
 
 echo $generator->MakeItHappenBabe();
-
